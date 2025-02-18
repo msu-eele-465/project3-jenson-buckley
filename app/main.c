5ce8f4568cee42db660aa654da65a1b3dcd5eba8
Peter Buckley <peterabbit22.22@gmail.com>
2025-02-18 12:07:44 -0700

    Completed Led Patterns Code

--- app/main.c
+++ app/main.c
@@ -12,14 +12,30 @@
 
 // Led Variables
 int stepIndex = 0;      // Current step index
-int direction = 1;      // 1 for clockwise, -1 for counterclockwise
-int stepsRemaining = 0; // Steps left to complete the step cycle
+int stepStart = 0;      // Start of the selected pattern
+int seqLength = 1;      // Length of sleected sequence
+
 unsigned char stepSequence[] = {
-    0x01, // Step 1: BIT0 (P6.0)
-    0x02, // Step 2: BIT1 (P6.1)
-    0x04, // Step 3: BIT2 (P6.2)
-    0x08  // Step 4: BIT3 (P6.3)
-};
+                // Pattern 0
+                0b0,
+                0b0,
+                // Pattern 1
+                0b00011000,
+                0b00100100,
+                0b01000010,
+                0b10000001,
+                0b01000010,
+                0b00100100,
+                // Pattern 2
+                0b1,
+                0b11,
+                0b111,
+                0b1111,
+                0b11111,
+                0b111111,
+                0b1111111,
+                0b11111111
+            };
 
 //--------------------------------------Main-------------------------------------//
 int main(void)
@@ -51,18 +67,17 @@ int main(void)
     // FUNCTION: function to read keypad input, given keypad struct
 
 //-- Setup patterns
-    // STRUCT: map pattern numbers to arrays holding 8-bits to be displayed at each step
-    // global to hold the current pattern array
-    // clock (setup, period, duty, clear/enable flag)
-    // ISR: ISR_PATTERN to display current pattern based on global and using LED array function
-    // (identical to above) FUNCTION: function for updating clock period, given clock and period in [0,255)
-
+    setupLeds();
+    setPattern(2);
 //-- GLOBAL ENABLES
     // enable GPIO
     // enable interrupts
     // Disable the GPIO power-on default high-impedance mdoe to activate
     // previously configure port settings
     PM5CTL0 &= ~LOCKLPM5;
+    __enable_interrupt();       // Enable global interrupt
+
+    while(true){}
 
 //-- WHILE TRUE:
     //-- Enter password
@@ -77,40 +92,44 @@ int main(void)
 }
 
 //------------------------------------Functions-----------------------------------//
-void setupMotor() {
-    // Configure Stepper Motor Outputs (P6.0 - P6.3)
-    P6DIR |= BIT0 | BIT1 | BIT2 | BIT3;
-    P6OUT &= ~(BIT0 | BIT1 | BIT2 | BIT3);
+void setupLeds() {
+    // Configure Leds (P6.0 - P6.4, P2.0 - P2.2)
+    P6DIR |= BIT0 | BIT1 | BIT2 | BIT3 | BIT4;
+    P6OUT &= ~(BIT0 | BIT1 | BIT2 | BIT3 | BIT4);
+    P2DIR |= BIT0 | BIT1 | BIT2;
+    P2OUT &= ~(BIT0 | BIT1 | BIT2);
     // Setup Timer B0
-    TB0CTL = TBSSEL__SMCLK | MC__STOP | TBCLR | ID__8; // SMCLK (1Mhz), Stop mode, clear timer, divide by 8
+    TB0CTL = TBSSEL__SMCLK | MC__UP | TBCLR | ID__8; // SMCLK (1Mhz), Stop mode, clear timer, divide by 8
     TB0EX0 = TBIDEX__4 ;   // Extra division by 4
     TB0CCR0 = 153;  // Set initial speed
-    TB0CCTL0 = CCIE;      // Enable compare interrupt
-    // Code to move motor
-    //if (stepsRemaining == 0) {  // Start a new rotation only if no rotation in progress
-    //    direction = 1;          // Clockwise
-    //    stepsRemaining = 739;   // 36% of a rotation
-    //    TB0CCR0 = 212;          // Set speed to do 36% of a rotation in 5sec
-    //    TB0CTL |= MC__UP;       // Start timer
-    //}
+    TB0CCTL0 |= CCIE;      // Enable compare interrupt
+}
+
+void setPattern(int a) {
+    switch (a) {
+        case 0:
+            stepStart = 0;
+            seqLength = 2;
+        break;
+        case 1:
+            stepStart = 2;
+            seqLength = 5;
+        break;
+        case 2:
+            stepStart = 8;
+            seqLength = 8;
+        break;
+    }
 }
 
 //---------------------------Interupt-Service-Routines---------------------------//
-// Timer for Motor
+// Timer for Led
 #pragma vector = TIMER0_B0_VECTOR
 __interrupt void ISR_TB0_CCR0(void)
 {
-    if (stepsRemaining > 0)
-    {
-        stepIndex = (stepIndex + 4 + direction) % 4; // Update step index
-        P6OUT = (P6OUT & 0xF0) | stepSequence[stepIndex]; // Update motor output
-        stepsRemaining--;   // Decrement steps remaining
-    }
-    else
-    {
-        TB0CTL &= ~MC__UP;  // Stop timer when all steps are completed
-        ADC_PressureReading();
-    }
+    stepIndex = (stepIndex + 1) % seqLength; // Update step index
+    P6OUT = (stepSequence[stepIndex + stepStart] & 0b00011111); // Update Led output
+    P2OUT = ((stepSequence[stepIndex + stepStart] >>5 ) & 0b00000111); // Update Led output
 
     TB0CCTL0 &= ~CCIFG;     // Clear interrupt flag
 }
