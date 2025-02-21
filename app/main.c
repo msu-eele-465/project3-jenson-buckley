d218da2b3a3087ad0ef31a2a337568b5026260b4
davidhjensen <dhjensen02@gmail.com>
2025-02-21 11:06:53 -0700

    woking keypad read

--- app/main.c
+++ app/main.c
@@ -10,16 +10,8 @@
 #include <msp430.h>
 #include <stdbool.h>
 
-// Led Variables
-int stepIndex = 0;      // Current step index
-int direction = 1;      // 1 for clockwise, -1 for counterclockwise
-int stepsRemaining = 0; // Steps left to complete the step cycle
-unsigned char stepSequence[] = {
-    0x01, // Step 1: BIT0 (P6.0)
-    0x02, // Step 2: BIT1 (P6.1)
-    0x04, // Step 3: BIT2 (P6.2)
-    0x08  // Step 4: BIT3 (P6.3)
-};
+char readKeypad();
+int checkRows();
 
 //--------------------------------------Main-------------------------------------//
 int main(void)
@@ -60,14 +52,14 @@ int main(void)
     P3DIR &= ~BIT5;
     P3DIR &= ~BIT1;
     P5REN |= BIT4;      // internal resistors
-    P5REN |= BIT4;
-    P5REN |= BIT4;
-    P5REN |= BIT4;
+    P1REN |= BIT1;
+    P3REN |= BIT5;
+    P3REN |= BIT1;
 	P5OUT &=~ BIT4;     // pull-downs
 	P1OUT &=~ BIT1;
 	P3OUT &=~ BIT5;
 	P3OUT &=~ BIT1;
-    // FUNCTION: function to read keypad input, given keypad struct
+    // FUNCTION: function to read keypad input
     //  readKeypad()
 
 //-- Setup patterns
@@ -85,29 +77,14 @@ int main(void)
     PM5CTL0 &= ~LOCKLPM5;
 
     while (true) {
-        
+        char val = readKeypad();
+        if (val == 0x1) {
+            char last_read = val;
+        }
     }
 }
 
 //------------------------------------Functions-----------------------------------//
-void setupMotor() {
-    // Configure Stepper Motor Outputs (P6.0 - P6.3)
-    P6DIR |= BIT0 | BIT1 | BIT2 | BIT3;
-    P6OUT &= ~(BIT0 | BIT1 | BIT2 | BIT3);
-    // Setup Timer B0
-    TB0CTL = TBSSEL__SMCLK | MC__STOP | TBCLR | ID__8; // SMCLK (1Mhz), Stop mode, clear timer, divide by 8
-    TB0EX0 = TBIDEX__4 ;   // Extra division by 4
-    TB0CCR0 = 153;  // Set initial speed
-    TB0CCTL0 = CCIE;      // Enable compare interrupt
-    // Code to move motor
-    //if (stepsRemaining == 0) {  // Start a new rotation only if no rotation in progress
-    //    direction = 1;          // Clockwise
-    //    stepsRemaining = 739;   // 36% of a rotation
-    //    TB0CCR0 = 212;          // Set speed to do 36% of a rotation in 5sec
-    //    TB0CTL |= MC__UP;       // Start timer
-    //}
-}
-
 char readKeypad() {
     // columns on P1.4, P5.3, P5.1, P5.0
     // rows on P5.4, P1.1, P3.5, 3.1
@@ -173,54 +150,15 @@ char readKeypad() {
 
 int checkRows() {
     // check inputs for rows on P5.4, P1.1, P3.5, 3.1 (rows 1-4)
-    int row_val = P5IN & BIT4;
-    if (row_val==1) {
+    if (P5IN & BIT4) {
         return 1;
-    }
-    int row_val = P1IN & BIT1;
-    if (row_val==1) {
+    } else if (P1IN & BIT1) {
         return 2;
-    }
-    int row_val = P3IN & BIT5;
-    if (row_val==1) {
+    } else if (P3IN & BIT5) {
         return 3;
-    }
-    int row_val = P3IN & BIT1;
-    if (row_val==1) {
+    } else if (P3IN & BIT1) {
         return 4;
+    } else {
+        return 0;
     }
-    return 0;
 }
-
-//---------------------------Interupt-Service-Routines---------------------------//
-// Timer for Motor
-#pragma vector = TIMER0_B0_VECTOR
-__interrupt void ISR_TB0_CCR0(void)
-{
-    if (stepsRemaining > 0)
-    {
-        stepIndex = (stepIndex + 4 + direction) % 4; // Update step index
-        P6OUT = (P6OUT & 0xF0) | stepSequence[stepIndex]; // Update motor output
-        stepsRemaining--;   // Decrement steps remaining
-    }
-    else
-    {
-        TB0CTL &= ~MC__UP;  // Stop timer when all steps are completed
-        ADC_PressureReading();
-    }
-
-    TB0CCTL0 &= ~CCIFG;     // Clear interrupt flag
-}
-
-//-- Heartbeat LED ISR
-    // toggle output
-    // clear flag
-
-//-- ISR_<[R, G, B]>
-    // turn on/off based on compare/overflow
-    // clear flag
-
-//-- ISR_PATTERN
-    // step pattern step# forward circularly
-    // write current step to output
-    // clear flag
